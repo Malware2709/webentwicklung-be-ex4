1b76eae04e8a9acdae2d55cf5a423b324275989f
Jan-Luca Höhn <146259878+Malware2709@users.noreply.github.com>
2024-06-06 19:43:17 +0000

    modified:   backend/db.js modified:   backend/index.js

--- backend/db.js
+++ backend/db.js
@@ -3,15 +3,18 @@ import { MongoClient, ObjectId } from 'mongodb';
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/products';
 const MONGO_DB = process.env.MONGO_DB || 'products';
 
+let db = null;
+let collection = null;
+let client = null;
 
 export default class DB {
     /** Connect to MongoDB and open client */
     connect() {
         return MongoClient.connect(MONGO_URI)
             .then((_client) => {
-                this.client = _client;
-                this.db = this.client.db(MONGO_DB);
-                this.collection = this.db.collection('products');
+                client = _client;
+                db = client.db(MONGO_DB);
+                collection = db.collection('products');
                 console.log("Connected to MongoDB");
             })
     }
@@ -20,14 +23,14 @@ export default class DB {
      * @returns {Promise} - Promise that resolves when connection is closed
     */
     close() {
-        return this.client.close()
+        return client.close()
     }
 
     /** Query all products from database
      * @returns {Promise} - Promise that resolves to an array of products
      */
     queryAll() {
-        return this.collection.find().toArray();
+        return collection.find().toArray();
     }
 
     /** Query a single product by id
@@ -35,7 +38,8 @@ export default class DB {
      * @returns {Promise} - Promise that resolves to a product object 
      */
     queryById(id) {
-        // TODO: Implement query by id
+        let _id = new ObjectId(id);
+        return collection.findOne({ _id });
     }
 
     /** Update product by id
@@ -43,7 +47,25 @@ export default class DB {
      * @returns {Promise} - Promise with updated product
      */
     update(id, product) {
-        //TODO: Implement update
+        let _id = new ObjectId(id);
+        if (typeof product._id === 'string') {
+            product._id = _id;
+        }
+        return collection
+            .replaceOne({ _id }, product)
+            .then(result => {
+                if (result.modifiedCount === 1 || result.matchedCount === 1) {
+                    return product;
+                }
+                else {
+                    console.log('Error updating product: %o, %s', result, id);
+                    throw new Error('Error updating product');
+                }
+            })
+            .catch(err => {
+                console.log('Error updating product: %o, %s', err, id);
+                throw err;
+            });
     }
 
     /** Delete product by id
@@ -51,7 +73,16 @@ export default class DB {
      * @returns {Promise} - Promise with deleted product
      */
     delete(id) {
-        // TODO: Implement delete
+        let _id = new ObjectId(id);
+        return collection.findOneAndDelete({ _id })
+            .then(result => {
+                if (result.ok) {
+                    return result.value;
+                } else {
+                    console.log('Error deleting product: %o, %s', result, id);
+                    throw new Error('Error deleting product');
+                }
+            })
     }
 
     /** Insert product
@@ -59,6 +90,17 @@ export default class DB {
      * @returns {Promise} - Promise with inserted product
      */
     insert(product) {
-        // TODO: Implement insert
+        return collection
+            .insertOne(product)
+            .then(result => {
+                if (result.acknowledged) {
+                    product._id = result.insertedId;
+                    return product;
+                }
+                else {
+                    console.log('Error inserting product: %o', result);
+                    throw new Error('Error inserting product');
+                }
+            });
     }
 }
