ad2b4758f2393be5f9bdca36356e0aa0ed14bd39
Jan-Luca Höhn <146259878+Malware2709@users.noreply.github.com>
2024-06-07 19:53:20 +0000

    modified:   backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,58 @@ const app = express();
 
 app.use(express.json());
 
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Produkt API',
+            version: '1.0.0',
+            description: 'Produkt API Dokumentation',
+        },
+        servers: [
+            {
+                url: 'http://127.0.0.1:3000',
+            },
+        ],
+        components: {
+            schemas: {
+                Produkt: {
+                    type: 'object',
+                    properties: {
+                        _id: {
+                            type: 'string',
+                            example: '6439519dadb77c080671a573',
+                        },
+                        name: {
+                            type: 'string',
+                            example: 'Mr. Krabs',
+                        },
+                        description: {
+                            type: 'string',
+                            example: 'Geiziger Restaurantbesitzer',
+                        },
+                        price: {
+                            type: 'float',
+                            example: '16.50',
+                        },
+                        stock: {
+                            type: 'integer',
+                        },
+                        image_url: {
+                            type: 'string',
+                            example: 'https://www.google.com',
+                        }
+                    },
+                },
+            },
+        },
+    },
+    apis: ['./index.js'],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 /** global instance of our database */
 let db = new DB();
 
@@ -29,8 +81,8 @@ const productValidationRules = [
         .isFloat({ min: 0.00 }).withMessage('Price must be a positive float'),
     check('stock')
         .isInt({ min: 0 }).withMessage('Stock must be a positive integer'),
-    //check('url')
-      //  .isURL({require_valid_protocol: false, require_tld: false, require_host: false}).withMessage('URL must be a valid URL'),
+    check('image_url')
+        .isURL({require_valid_protocol: false, require_tld: false, require_host: false}).withMessage('URL must be a valid URL'),
     checkExact([], {locations: ['body'], message: 'No additional fields allowed'}),
 ];
 
@@ -40,21 +92,60 @@ const productValidationRules = [
 /** Return all products. 
  *  Be aware that the db methods return promises, so we need to use either `await` or `then` here! 
  * 
+ * @swagger
+ * /products:
+ *  get:
+ *    summary: Gibt alle Produkte zurück
+ *    tags: [Produkte]
+ *    responses:
+ *      '200':
+ *        description: Eine Liste aller Produkte
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Produkt'
  */
 app.get('/products', async (req, res) => {
     let products = await db.queryAll();
-    res.send(products);
+    res.status(200).send(products);
 });
 
 
 /** Return a single customer by id 
  * 
+ * @swagger
+ * /products/{id}:
+ *  get:
+ *    summary: Gibt ein bestimmtes Produkt anhand der mitgegebenen ID aus
+ *    tags: [Produkte]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *          required: true
+ *          description: Die ID des Produkts
+ *    responses:
+ *      '200':
+ *        description: Das gesuchte Produkt
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Produkt'
+ *      '404':
+ *        description: Produkt wurde nicht gefunden
+ *      '500':
+ *        description: Ein Fehler ist aufgetreten
  */
 app.get('/products/:id', async (req, res) => {
     return db.queryById(req.params.id)
         .then(product => {
             if (product) {
-                res.send(product);
+                res.status(200).send(product);
             } else {
                 res.status(404).send("Product not found");
             }
@@ -67,6 +158,28 @@ app.get('/products/:id', async (req, res) => {
 
 /** Create a new customer
  *
+ * @swagger
+ * /products:
+ *  post:
+ *    summary: Erstellt ein neues Produkt
+ *    tags: [Produkte]
+ *    requestBody:
+ *     required: true
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/Produkt'
+ *    responses:
+ *     '201':
+ *       description: Das erstellte Produkt
+ *       content:
+ *        application/json:
+ *         schema:
+ *          $ref: '#/components/schemas/Produkt'
+ *     '400':
+ *       description: Ungültige Eingabe
+ *     '500':
+ *       description: Ein Fehler ist aufgetreten
  */
 app.post('/products', productValidationRules, async (req, res) => {
     let result = validationResult(req);
@@ -87,6 +200,37 @@ app.post('/products', productValidationRules, async (req, res) => {
 
 /** Update an existing customer
  * 
+ * @swagger
+ * /products/{id}:
+ *  put:
+ *   summary: Aktualisiert ein bestehendes Produkt
+ *   tags: [Produkte]
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *       type: string
+ *       required: true
+ *       description: Die ID des Produkts
+ *   requestBody:
+ *    required: true
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/components/schemas/Produkt'
+ *   responses:
+ *    '200':
+ *     description: Das aktualisierte Produkt
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/Produkt'
+ *    '404':
+ *     description: Produkt wurde nicht gefunden
+ *    '400':
+ *     description: Ungültige Eingabe
+ *    '500':
+ *     description: Ein Fehler ist aufgetreten
  */
 app.put('/products/:id', productValidationRules, async (req, res) => {
     let result = validationResult(req);
@@ -110,6 +254,29 @@ app.put('/products/:id', productValidationRules, async (req, res) => {
 
 /** Delete a customer by id
  *
+ * @swagger
+ * /products/{id}:
+ *  delete:
+ *   summary: Löscht ein Produkt
+ *   tags: [Produkte]
+ *   parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *       type: string
+ *       required: true
+ *       description: Die ID des zu löschenden Produkts
+ *   responses:
+ *    '200':
+ *     description: Das gelöschte Produkt
+ *     content:
+ *      application/json:
+ *       schema:
+ *        $ref: '#/components/schemas/Produkt'
+ *    '404':
+ *     description: Produkt wurde nicht gefunden
+ *    '500':
+ *     description: Ein Fehler ist aufgetreten
  */
 app.delete('/products/:id', async (req, res) => {
     return db.delete(req.params.id)
